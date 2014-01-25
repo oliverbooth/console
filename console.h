76a6840ab21d1d4ef2c5517aca734724fe20297d
Oliver Davenport <yasahiro93@gmail.com>
2014-01-25 17:43:20 +0000

    Added clearx(int, int) and cleary(int, int);

--- console.h
+++ console.h
@@ -11,54 +11,59 @@
 
 #define COLOR_RESET  -1
 #define COLOR_BLACK   0
-#ifdef OS_Windows
-// Windows is difficult. Colors are different
-#define COLOR_AQUA    3
-#define COLOR_BLUE    1
-#define COLOR_CYAN    3
-#define COLOR_RED     4
-#define COLOR_YELLOW  6
-#else
-// Proper ANSI colors :)
-#define COLOR_AQUA    6
-#define COLOR_BLUE    4
-#define COLOR_CYAN    6
-#define COLOR_RED     1
-#define COLOR_YELLOW  3
-#endif
 #define COLOR_GREEN   2
 #define COLOR_MAGENTA 5
 #define COLOR_FUSCHIA 5
 #define COLOR_PURPLE  5
 #define COLOR_WHITE   7
 #define COLOR_BRIGHT  8
+#ifdef OS_Windows
+#define COLOR_AQUA    3
+#define COLOR_BLUE    1
+#define COLOR_CYAN    3
+#define COLOR_RED     4
+#define COLOR_YELLOW  6
+#else
+#define COLOR_AQUA    6
+#define COLOR_BLUE    4
+#define COLOR_CYAN    6
+#define COLOR_RED     1
+#define COLOR_YELLOW  3
+#endif
 
 // BRIGHT
-#define COLOR_GRAY          (COLOR_BRIGHT | COLOR_BLACK)
+#define COLOR_BRIGHTAQUA    (COLOR_BRIGHT | COLOR_AQUA)
 #define COLOR_BRIGHTBLACK   (COLOR_BRIGHT | COLOR_BLACK)
 #define COLOR_BRIGHTBLUE    (COLOR_BRIGHT | COLOR_BLUE)
-#define COLOR_BRIGHTGREEN   (COLOR_BRIGHT | COLOR_GREEN)
-#define COLOR_BRIGHTAQUA    (COLOR_BRIGHT | COLOR_AQUA)
 #define COLOR_BRIGHTCYAN    (COLOR_BRIGHT | COLOR_CYAN)
-#define COLOR_BRIGHTRED     (COLOR_BRIGHT | COLOR_RED)
-#define COLOR_BRIGHTMAGENTA (COLOR_BRIGHT | COLOR_MAGENTA)
 #define COLOR_BRIGHTFUSCHIA (COLOR_BRIGHT | COLOR_FUSCHIA)
+#define COLOR_BRIGHTGREEN   (COLOR_BRIGHT | COLOR_GREEN)
+#define COLOR_BRIGHTMAGENTA (COLOR_BRIGHT | COLOR_MAGENTA)
 #define COLOR_BRIGHTPURPLE  (COLOR_BRIGHT | COLOR_PURPLE)
-#define COLOR_BRIGHTYELLOW  (COLOR_BRIGHT | COLOR_YELLOW)
+#define COLOR_BRIGHTRED     (COLOR_BRIGHT | COLOR_RED)
 #define COLOR_BRIGHTWHITE   (COLOR_BRIGHT | COLOR_WHITE)
+#define COLOR_BRIGHTYELLOW  (COLOR_BRIGHT | COLOR_YELLOW)
+#define COLOR_GRAY          (COLOR_BRIGHT | COLOR_BLACK)
 
-// 
+/* Resets all terminal parameters. */
+void resetall();
 
-/* Gets the width of the console buffer.
+/* Gets the width of the terminal buffer.
 Written by Oliver Davenport (12033278) */
 int getwidth();
 
-/* Gets the height of the console buffer.
+/* Gets the height of the terminal buffer.
 Written by Oliver Davenport (12033278) */
 int getheight();
 
-/* Clears the console buffer. */
+/* Clears the terminal buffer. */
 void clrscr();
+
+/* Clears the X line on row Y. */
+void clearx(int y, int offset = 0);
+
+/* Clears the Y line on column X. */
+void cleary(int x, int offset = 0);
 
 /* Sleep for a number of milliseconds. */
 void pause(int time);
@@ -70,19 +75,19 @@ void gotoxy(int x, int y);
 Written by Oliver Davenport (12033278) */
 void wherexy(int &x, int &y);
 
-/* Sets the console background color. */
+/* Sets the terminal background color. */
 void setbg(int color);
 
-/* Sets the console foreground color. */
+/* Sets the terminal foreground color. */
 void setfg(int color);
 
-/* Sets the console foreground and background color. */
+/* Sets the terminal foreground and background color. */
 void setrgb(int fg, int bg = -2);
 
 /* Shows or hides the cursor */
 void showcursor(bool show = true);
 
-/* Wait for a key to be pressed on the console.
+/* Wait for a key to be pressed in the terminal.
 Written by user firedraco @ http://cplusplus.com/forum */
 int waitkey(const char* prompt = "");
 
